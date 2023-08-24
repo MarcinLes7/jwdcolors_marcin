c1e42a8d84f53ffa308c7621e32426b6a51d7940
Marcin <lmartino@wp.pl>
2023-08-24 06:10:53 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/jwdcolors_marcin/JwdcolorsMarcinApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwdcolors_marcin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwdcolorsMarcinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwdcolorsMarcinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/jwdcolors_marcin/JwdcolorsMarcinApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwdcolors_marcin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwdcolorsMarcinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
